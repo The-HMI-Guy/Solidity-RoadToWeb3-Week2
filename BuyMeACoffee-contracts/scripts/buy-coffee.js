6bd939cc3cd78d27c1aa3f3d6c5fdc823a32f0c8
Ray Ortiz <oray@bgsu.edu>
2022-08-08 18:59:36 -0400

    work on scripts

--- BuyMeACoffee-contracts/scripts/buy-coffee.js
+++ BuyMeACoffee-contracts/scripts/buy-coffee.js
@@ -6,6 +6,33 @@
 // global scope, and execute the script.
 const hre = require("hardhat");
 
+// Returns the Ether balance of a given address.
+async function getBalance(address) {
+  const balanceBigInt = await hre.waffle.provider.getBalance(address);
+  return hre.ethers.utils.formatEther(balanceBigInt);
+}
+
+// Logs the Ether balances for a list of addresses
+async function printBalances(address) {
+  let idx = 0;
+  for (const address of addresses) {
+    console.log(`Adress ${idx} balance: `, await getBalance(address));
+    idx++;
+  }
+}
+// Logs the memos stored on-chain from coffee purchases.
+async function printMemos(memos) {
+  for (const memo of memos) {
+    const timestamp = memo.timestamp;
+    const tipper = memo.name;
+    const tipperAddress = memo.from;
+    const message = memo.message;
+    console.log(
+      `At ${timestamp}, ${tipper} (${tipperAddress}) said: "${message}"`
+    );
+  }
+}
+
 async function main() {
   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
   const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
