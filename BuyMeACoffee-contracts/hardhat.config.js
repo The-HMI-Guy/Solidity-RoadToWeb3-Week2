82dca74e5594a8fa709b6c01df2950648782e4fb
Ray Ortiz <oray@bgsu.edu>
2022-08-10 07:35:01 -0400

    setting up the config for the deploy script

--- BuyMeACoffee-contracts/hardhat.config.js
+++ BuyMeACoffee-contracts/hardhat.config.js
@@ -1,8 +1,20 @@
+const { config } = require("hardhat");
+
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-chai-matchers");
 require("@nomiclabs/hardhat-ethers");
+require("dotenv")/config();
+
+const GOERLI_URL = process.env.GOERLI_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: GOERLI_URL,
+      account: [PRIVATE_KEY]
+    }
+  }
 };
